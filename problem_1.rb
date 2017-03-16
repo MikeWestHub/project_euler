f2dccb499eeb1f61182e4b4a5a2c920e095ddfad
Mike West <michael.west@phishme.com>
2017-03-16 15:52:03 -0400

    First Ruby solution for Problem 1

---
+++ problem_1.rb
@@ -0,0 +1,25 @@
+class GetRange
+  attr_reader :range 
+
+  def initialize(range)
+    @range = range
+  end
+
+  def find_multiples
+    numbers = []
+    range.each do |n|
+      if n % 3 == 0 || n % 5 == 0 
+        numbers << n
+      end
+    end
+    numbers
+  end
+
+  def sum_of_range
+    find_multiples.reduce(0, :+)
+  end
+end
+
+range = GetRange.new(1..1000)
+answer = range.sum_of_range
+puts answer
