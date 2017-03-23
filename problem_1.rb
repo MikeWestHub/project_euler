44f41104ad07ab27cc8a6e0bad9d6f51fae800f1
Mike West <michael.west@phishme.com>
2017-03-23 14:22:49 -0400

    Use select instead of each to modify range array

--- problem_1.rb
+++ problem_1.rb
@@ -1,25 +1,18 @@
-class GetRange
+class MultiplesOfThreeAndFive
   attr_reader :range 
 
   def initialize(range)
     @range = range
   end
 
-  def find_multiples
-    numbers = []
-    range.each do |n|
-      if n % 3 == 0 || n % 5 == 0 
-        numbers << n
-      end
-    end
-    numbers
+  def filter_range
+    range.select { |n| (n % 3 == 0) || (n % 5 == 0) }
   end
-
+  
   def sum_of_range
-    find_multiples.reduce(0, :+)
+    filter_range.reduce(0, :+)
   end
 end
 
-range = GetRange.new(1...1000)
-answer = range.sum_of_range
+answer = MultiplesOfThreeAndFive.new(1...1000).sum_of_range
 puts answer
